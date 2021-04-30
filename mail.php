84b810e94a382ab36df927bcf144598544c2fa7b
charlesben28 <83427141+charlesben28@users.noreply.github.com>
2021-04-30 07:35:44 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -9,17 +9,10 @@
     $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-    $message ='<table style="width:100%">
-        <tr>
-            <td>'.$name.'  '.$subject.'</td>
-        </tr>
-        <tr><td>Email: '.$email.'</td></tr>
-        <tr><td>phone: '.$subject.'</td></tr>
-        <tr><td>Text: '.$text.'</td></tr>
-        
-    </table>';
-
+    
+    $message=" "
+    $message.='A NEW MESSAGE FROM '.$_POST['name'].' via '.$_POST['email'].'<br />';    
+    $message.=$_POST['message'];
     if (@mail($to, $email, $message, $headers))
     {
         echo 'The message has been sent.';
